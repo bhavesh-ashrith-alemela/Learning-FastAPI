e709d9ba643ccaadb38a5ef5c5d9e493485a9666
bhavesh-ashrith-alemela <bhavesh.alemela@gmail.com>
2026-06-20 16:42:33 +0530

    Learned GET API

--- main.py
+++ main.py
@@ -3,39 +3,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Address(BaseModel):
-    city: str
-    zip_code: str
+todos = []
 
-class User(BaseModel):
-    name: str
-    age: int
-    address: Address
+class Todo(BaseModel):
+    id : int
+    title : str
+    completed : bool
 
-#Home Route
-@app.get("/")
-def home():
-    return {"message": "Welcome to the FastAPI"}
 
-#About Route
-@app.get("/about")
-def about():
-    return {"message": "This is an about page."}
+@app.post("/todos")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo added", "data": todo}
 
-#defined users route
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id}
+@app.get("/todos")
+def  get_todos():
+    return {"data": todos}
 
-#Query Parameters
-@app.get("/users")
-def get_users(name: str = None):
-    return {"name": name}
-
-@app.get("/items")
-def get_items(name: str = None, price: int=0):
-    return {"name": name,"price": price}
-
-@app.post("/create_user")
-def create_user(user: User):
-    return {"message": "User created successfully", "user": user}
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"data": todo}
+        return {"error": "Todo not found"}
+    
