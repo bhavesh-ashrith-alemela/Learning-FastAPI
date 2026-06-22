bb3f3d2e5836ec8f4e28d95325e6f088aa6980c2
bhavesh-ashrith-alemela <bhavesh.alemela@gmail.com>
2026-06-22 15:52:11 +0530

    Learned how to use path + query + body together

--- main.py
+++ main.py
@@ -3,39 +3,28 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Address(BaseModel):
-    city: str
-    zip_code: str
+users = []
 
 class User(BaseModel):
     name: str
     age: int
-    address: Address
 
-#Home Route
-@app.get("/")
-def home():
-    return {"message": "Welcome to the FastAPI"}
+# PUT /users/101?notify=true
 
-#About Route
-@app.get("/about")
-def about():
-    return {"message": "This is an about page."}
+#{
+#    "name": "John Doe",
+#    "email": "john.doe@example.com"
+#}
 
-#defined users route
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id}
-
-#Query Parameters
-@app.get("/users")
-def get_users(name: str = None):
-    return {"name": name}
-
-@app.get("/items")
-def get_items(name: str = None, price: int=0):
-    return {"name": name,"price": price}
-
-@app.post("/create_user")
+@app.post("/users")
 def create_user(user: User):
+    users.append(user)
     return {"message": "User created successfully", "user": user}
+
+@app.put("/users/{user_id}")
+def updated_user(user_id: int, user: User, notify: bool = False):
+    if user_id < len(users):
+        users[user_id] = user
+        
+        return {"message": "User updated successfully", "data": user, "notify": notify}
+    return {"error": "User not found"}
