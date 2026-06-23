cd224ce10aa75dfe29f2b1db71329750975e0c76
bhavesh-ashrith-alemela <bhavesh.alemela@gmail.com>
2026-06-23 16:36:20 +0530

    JWT noraml auth & OAuth2 high secure auth done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException, Request, Depends, Header
+from fastapi import FastAPI, status, HTTPException, Request, Depends, Header, UploadFile, File
 import sqlite3
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
@@ -6,8 +6,181 @@ import time
 import asyncio
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from fastapi.staticfiles import StaticFiles
+import os
+import shutil
 
 app = FastAPI()
+
+#JWT Config
+SECRET_KEY = "mysecret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRY_MINUTES = 30
+
+#Password hashing setup
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+
+#Oauth Setup
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")
+
+#Dummy user DB
+fake_user_db = {
+    "admin": {
+        "username": "admin",
+        "hashed_password": pwd_context.hash("1234")
+    }
+}
+
+#Hash password
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+#verify password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+#Create Token
+def create_token(data: dict):
+    to_encode = data.copy()
+    expiry = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRY_MINUTES)
+    to_encode.update({
+        "exp": expiry
+    })
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+#Login API token generator 
+# @app.post("/login")
+# def login(user_name: str, password: str):
+#     if user_name != "admin" or password != "1234":
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Invalid User name & Password"
+#         )
+#     token = create_token({
+#         "sub": user_name
+#     })
+#     return {
+#         "access_token": token,
+#         "token_type": "bearer"
+#     }
+
+#Login API OAuth
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = fake_user_db.get(form_data.username)
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid username or password"
+        )
+    access_token = create_token({"sub":form_data.username})
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+#Token verify
+# def verify_token(authorization: str = Header(None), token: str = Header(None)):
+#     if authorization is None and token is None:
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Authorization header missing"
+#         )
+
+#     if authorization is not None:
+#         scheme, _, token_value = authorization.partition(" ")
+#         if scheme.lower() != "bearer" or not token_value:
+#             raise HTTPException(
+#                 status_code=401,
+#                 detail="Invalid authorization scheme"
+#             )
+#     else:
+#         token_value = token
+
+#     try:
+#         payload = jwt.decode(token_value, SECRET_KEY, algorithms=[ALGORITH])
+#         return payload
+#     except Exception:
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Invalid or expired Token"
+#         )
+
+def verify_token(token:str = Depends(oauth2_schema)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username:str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+        return username
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
+
+#protected route
+# @app.get("/security")
+# def secure_data(user = Depends(verify_token)):
+#     return {
+#         "message": "Secure data accessed",
+#         "user": user
+#     }
+
+@app.get("/protected")
+def protected_route(username:str = Depends(verify_token)):
+    return {
+        "message": f"Hello {username}, you have access to this protected route!",
+        "user": username
+    }
+
+#Step-1 ensure upload folder exists
+UPLOAD_DIR = "uploads"
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
+#Step-2 Static file stepup
+#Url : http://127.0.0.1:8080/files/<filename>
+app.mount("/files", StaticFiles(directory=UPLOAD_DIR), name="files")
+
+#Step-3 upload file api
+@app.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+    filename = file.filename
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    if not filename:
+        raise HTTPException(
+            status_code=400,
+            detail="File not selected"
+        )
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+        return {
+            "message": "File uploaded successfully",
+            "fileName": filename,
+            "file_url": f"http://127.0.0.1:8000/files/{filename}"
+        }
+    
+#step-4 get file url api
+@app.get("/files/{filename}")
+def  get_file(filename:str):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+    return {
+        "file url": f"http://127.0.0.1:8000/files/{filename}"
+    }
 
 #con = sqlite3.connect("test.db", check_same_thread=False)
 
@@ -68,16 +241,6 @@ async def log_middleware(request: Request, call_next):
 
     return response
 
-def verify_token(token: str = Header(None)):
-    if token != "mysecrettoken":
-        raise HTTPException(
-            status_code=401,
-            detail="Unauthorized"
-        )
-    return {
-        "user": "Authorized user"
-    }
-
 def common_logic():
     return {
         "message": "common logic executed"
@@ -109,7 +272,7 @@ def user_not_found(request:Request, exc:UserNotFoundException):
 #Home Route
 @app.get("/")
 def home():
-    return {"message": "Welcome to the FastAPI, DB Connected fine"}
+    return {"message": "Welcome to the FastAPI, DB Connected fine, File uploaded api running"}
 
 #Create API
 @app.post("/todos")
@@ -120,7 +283,11 @@ def create_todo(title:str, db:Session = Depends(get_db)):
     db.refresh(todo)
     return {
         "message": "Todo created",
-        "data": todo
+        "data": {
+            "id": todo.id,
+            "title": todo.title,
+            "completed": todo.completed
+        }
     }
 
 #Read all data
@@ -129,7 +296,10 @@ def get_todos(db:Session = Depends(get_db)):
     todos = db.query(Todo).all()
     return {
         "Total": len(todos),
-        "data": todos
+        "data": [
+            {"id": todo.id, "title": todo.title, "completed": todo.completed}
+            for todo in todos
+        ]
     }
 
 #Read data by Id
@@ -143,7 +313,11 @@ def get_todo(todo_id:int, db:Session = Depends(get_db)):
         )
     return {
         "message": "Found Todo",
-        "data": todo
+        "data": {
+            "id": todo.id,
+            "title": todo.title,
+            "completed": todo.completed
+        }
     }
 
 #Update data by id
@@ -160,7 +334,11 @@ def put_todo(todo_id:int, title:str, db:Session = Depends(get_db)):
     db.refresh(todo)
     return {
         "message": "Todo updated",
-        "data": todo
+        "data": {
+            "id": todo.id,
+            "title": todo.title,
+            "completed": todo.completed
+        }
     }
 
 #Delete data
@@ -180,7 +358,7 @@ def del_todo(todo_id:int, db:Session = Depends(get_db)):
 
 #Query Parameters
 @app.get("/users")
-def get_users(name: str = None):
+def get_users_list(name: str = None):
     return {"name": name}
 
 @app.get("/items")
@@ -192,7 +370,7 @@ def create_user(user: User):
     return {"message": "User created successfully", "user": user}
 
 @app.get("/user")
-def get_user():
+def get_user_single():
     return {
         "status": "Success",
         "message": "User Fetched",
@@ -215,7 +393,7 @@ def get_users(user_id: int):
     }
 
 @app.get("/clients/{name}")
-def get_user(name:str):
+def get_client_by_name(name:str):
     if name != "bhavesh":
         raise UserNotFoundException(name)
     return {
@@ -223,7 +401,7 @@ def get_user(name:str):
     }
 
 @app.get("/client/{user_id}")
-def get_user(user_id: int):
+def get_client_by_id(user_id: int):
     if user_id != 1:
         raise HTTPException(
             status_code=404,
@@ -235,7 +413,7 @@ def get_user(user_id: int):
     }
 
 @app.get("/home")
-def home(data= Depends(common_logic)):
+def home_with_data(data= Depends(common_logic)):
     return data
 
 @app.get("/profile")
