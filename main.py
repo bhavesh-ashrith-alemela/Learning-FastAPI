086c072a1cd586db6fb3b8dab90e3f23668060b9
bhavesh-ashrith-alemela <bhavesh.alemela@gmail.com>
2026-06-25 14:57:16 +0530

    Learned rate limiting api and caching

--- main.py
+++ main.py
@@ -17,9 +17,34 @@ from fastapi.middleware.cors import CORSMiddleware
 # from dotenv import load_dotenv
 from config import settings
 import requests
-
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+#limiter setup
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+
+#Error handling
+@app.exception_handler(RateLimitExceeded)
+def rate_limit_handler(request:Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={
+            "detail": "TOO Many Requests"
+        }
+    )
+
+#Rate Limiter API
+@app.get("/data")
+@limiter.limit("5/minute")
+def get_data(request:Request):
+    return {
+        "message": "success"
+    }
 
 # load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
